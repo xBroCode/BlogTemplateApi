836d0a2450d0ed694ddcd04b72c49df0b1034255
Icaro Falcao <vbsfalcao@gmail.com>
2020-05-31 23:20:59 -0300

    Add swagger configuration

--- src/BroCode.BlogTemplate.WebApi/Startup.cs
+++ src/BroCode.BlogTemplate.WebApi/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace BroCode.BlogTemplate.WebApi
 {
@@ -21,6 +23,21 @@ namespace BroCode.BlogTemplate.WebApi
         {
             services.AddControllers();
             DependencyResolver.ConfigureServices(services, Configuration);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Blog Template API",
+                    Description = "Template for easy creation of blogs",
+                    Version = "1.0.0-alpha1",
+                    Contact = new OpenApiContact 
+                    { 
+                        Name = "Ícaro Falcão", 
+                        Email = "vbsfalcao@gmail.com", 
+                        Url = new Uri("https://ifalcao.live") 
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +57,12 @@ namespace BroCode.BlogTemplate.WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlogTemplate");
             });
         }
     }
